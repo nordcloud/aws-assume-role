178cf12ede7d06ff43ccb033490e87fda7550f9d
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2019-12-02 10:08:36 +0100

    Testing better path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cd /
+
 if [ -z "$AWS_ACCESS_KEY_ID" ]; then
   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
   exit 1
@@ -11,11 +13,8 @@ if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
   exit 1
 fi
-# ldd /assume-role-arn
-stat /assume-role-arn
-file /assume-role-arn
 
-eval $(./assume-role-arn $*)
+eval $(/assume-role-arn $*)
 
 echo "::set-env name=AWS_ACCESS_KEY_ID::${AWS_ACCESS_KEY_ID}"
 echo "::set-env name=AWS_SECRET_ACCESS_KEY::${AWS_SECRET_ACCESS_KEY}"
