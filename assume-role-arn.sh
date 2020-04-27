20cda50f0cde38f0cfd46ad3a153db83b01733b8
tetsuya-zama <tetsuya.zama@gmail.com>
2020-04-27 17:27:25 +0900

    Add masking generated environment values

--- assume-role-arn.sh
+++ assume-role-arn.sh
@@ -42,4 +42,7 @@ if [ -n "$AWS_ACCESS_KEY_ID" ]; then
     echo "::set-env name=AWS_ACCESS_KEY_ID::${AWS_ACCESS_KEY_ID}"
     echo "::set-env name=AWS_SECRET_ACCESS_KEY::${AWS_SECRET_ACCESS_KEY}"
     echo "::set-env name=AWS_SESSION_TOKEN::${AWS_SESSION_TOKEN}"
+    echo "::add-mask::${AWS_ACCESS_KEY_ID}"
+    echo "::add-mask::${AWS_SECRET_ACCESS_KEY}"
+    echo "::add-mask::${AWS_SESSION_TOKEN}"
 fi
